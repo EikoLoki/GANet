c7912dd1a8ec3099a7fb5d2aa2d6de4b3f9cc2ab
eikoloki <imdawnstar@gmail.com>
2019-08-20 00:13:33 -0400

    build finished

--- compile.sh
+++ compile.sh
@@ -6,23 +6,23 @@
 #export CUDNN_INCLUDE_DIR="/usr/local/cuda-10.0/include"
 #export CUDNN_LIB_DIR="/usr/local/cuda-10.0/lib64"
 
-#export LD_LIBRARY_PATH="/home/zhangfeihu/anaconda3/lib:$LD_LIBRARY_PATH"
-#export LD_INCLUDE_PATH="/home/zhangfeihu/anaconda3/include:$LD_INCLUDE_PATH"
-#export CUDA_HOME="/home/work/cuda-9.2"
-#export PATH="/home/zhangfeihu/anaconda3/bin:/home/work/cuda-9.2/bin:$PATH"
-#export CPATH="/home/work/cuda-9.2/include"
-#export CUDNN_INCLUDE_DIR="/home/work/cudnn/cudnn_v7/include"
-#export CUDNN_LIB_DIR="/home/work/cudnn/cudnn_v7/lib64"
-TORCH=$(python -c "import os; import torch; print(os.path.dirname(torch.__file__))")
+export LD_LIBRARY_PATH="/usr/local/lib/python3/dist-packages:/usr/local/cuda/lib64:$LD_LIBRARY_PATH"
+export LD_INCLUDE_PATH="/usr/include/python3.6:/usr/local/cuda/include$LD_INCLUDE_PATH"
+export CUDA_HOME="/usr/local/cuda"
+export PATH="/usr/bin:/usr/local/cuda/bin:$PATH"
+export CPATH="/usr/local/cuda/include"
+export CUDNN_INCLUDE_DIR="/usr/local/cuda/include"
+export CUDNN_LIB_DIR="/usr/local/cuda/lib64"
+TORCH=$(python3 -c "import os; import torch; print(os.path.dirname(torch.__file__))")
 #echo $TORCH
 cd libs/GANet
-python setup.py clean
+python3 setup.py clean
 rm -rf build
-python setup.py build
+python3 setup.py build
 cp -r build/lib* build/lib
 
 cd ../sync_bn
-python setup.py clean
+python3 setup.py clean
 rm -rf build
-python setup.py build
+python3 setup.py build
 cp -r build/lib* build/lib
